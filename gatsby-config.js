3aa75f1dd45ccdb657f4d32e7c658cabff974e92
Justin Andrade <42485697+JustinAndrade@users.noreply.github.com>
2024-05-18 18:10:19 -0400

    fix things

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,10 @@ module.exports = {
   siteMetadata: {
     title: 'Justin Andrade',
     description:
-      'Justin Andrade is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Justin Andrade is a software engineer who specializes in building exceptional digital experiences.',
+    siteUrl: 'https://juscoding.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@juscoding',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -151,9 +151,9 @@ module.exports = {
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'UA-45666519-2',
-      },
+      // options: {
+      //   trackingId: 'UA-45666519-2',
+      // },
     },
   ],
 };
