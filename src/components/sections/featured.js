02a9b0a769208eb3df6c038193408bd56a4ff831
Justin Andrade <42485697+JustinAndrade@users.noreply.github.com>
2024-05-29 16:33:28 -0400

    fix background color

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -260,7 +260,7 @@ const StyledProject = styled.li`
     a {
       width: 100%;
       height: 100%;
-      background-color: var(--green);
+      background-color: rgba(0, 0, 0, 0.2);
       border-radius: var(--border-radius);
       vertical-align: middle;
 
